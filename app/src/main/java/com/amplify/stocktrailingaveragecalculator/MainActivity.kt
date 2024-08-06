76a1be24c97f83fe436e16de7f45239afd2e81db
ezbuckeye <105604551+ezbuckeye@users.noreply.github.com>
2024-08-06 13:50:01 -0500

    added two reference days as common on top of mainactivity

--- app/src/main/java/com/amplify/stocktrailingaveragecalculator/MainActivity.kt
+++ app/src/main/java/com/amplify/stocktrailingaveragecalculator/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.amplify.stocktrailingaveragecalculator.ui.theme.StockTrailingAverageCalculatorTheme
 
+// July 12
+// August 9
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
